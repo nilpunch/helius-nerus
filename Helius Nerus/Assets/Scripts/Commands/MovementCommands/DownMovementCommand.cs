c21fabfa9eb5389a59ef600849f0c12b40291bb0
BulkaWithMaslo <slojnoblet@gmail.com>
2020-09-25 12:06:25 +0300

    Add movement commands and processor

---
+++ Helius Nerus/Assets/Scripts/Commands/MovementCommands/DownMovementCommand.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class DownMovementCommand : MovementCommand
+{
+    private float _timeElapsed = 0.0f;
+
+    public DownMovementCommand(float movementMult, float timeScale, float endParam, bool workOnce) : base(movementMult, timeScale, endParam, workOnce)
+    {
+        _timeElapsed = 0.0f;
+    }
+
+    public DownMovementCommand()
+    {
+        _timeElapsed = 0.0f;
+    }
+
+
+    public override bool IsEnded()
+    {
+        if (_timeElapsed > _endParameter)
+            return true;
+        return false;
+    }
+
+    public override void Tick(Transform ship)
+    {
+        _timeElapsed += Time.deltaTime * _timeScale;
+        ship.Translate(Vector3.down * _movementMultiplier * Time.deltaTime * _timeScale, Space.World);
+    }
+
+    public override void Reset()
+    {
+        _timeElapsed = 0.0f;
+    }
+}
---
+++ Helius Nerus/Assets/Scripts/Commands/MovementCommands/TemporaryEnemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TemporaryEnemy : MonoBehaviour
+{
+    [SerializeField] private MovementCommandsProcessor _processor = new MovementCommandsProcessor();
+
+    private void Awake()
+    {
+        _processor.Initialize(transform);
+    }
+
+    private void Update()
+    {
+        _processor.Tick();
+    }
+}
