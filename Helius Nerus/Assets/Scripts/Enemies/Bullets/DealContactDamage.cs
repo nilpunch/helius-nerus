c62a01d5fc78512c77a75402adaa1bbedff0f106
BulkaWithMaslo <slojnoblet@gmail.com>
2020-09-28 12:07:11 +0300

    Add basic bullets classes + renamed enemies folder

---
+++ Helius Nerus/Assets/Scripts/Enemies/Bullets/DealContactDamage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DealContactDamage : MonoBehaviour
+{
+    public int Damage
+    {
+        set => _damage = value;
+    }
+
+    private int _damage = 1;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Deal damage to player
+        // Destroy itself
+    }
+}
---
+++ Helius Nerus/Assets/Scripts/Enemies/Bullets/StraightMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class StraightMove : MonoBehaviour, IBulletMovement
+{
+    public float SpeedMultiplier
+    {
+        set => _speedMultiplier = value;
+    }
+    private float _speedMultiplier = 1.0f;
+    private Transform _transform = null;
+
+    private void Awake()
+    {
+        _transform = transform;
+    }
+
+    private void Update()
+    {
+        _transform.Translate(Vector3.up * Time.deltaTime * _speedMultiplier, Space.Self);
+    }
+}
