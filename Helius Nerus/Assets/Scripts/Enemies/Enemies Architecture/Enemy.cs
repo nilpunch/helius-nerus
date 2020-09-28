6b25090ce784494af7d5c39c46000f8f2e1368e4
BulkaWithMaslo <slojnoblet@gmail.com>
2020-09-28 11:29:54 +0300

    Add commented base for enemy

---
+++ Helius Nerus/Assets/Scripts/Enemies/Enemies Architecture/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private EnemyStats _stats = null;
+    [SerializeField] private CommandsProcessor<MoveCommandParameters> _moveProcessor = new CommandsProcessor<MoveCommandParameters>();
+    [SerializeField] private CommandsProcessor<WeaponCommandParameters> _weaponProcessor = new CommandsProcessor<WeaponCommandParameters>();
+
+    private void Awake()
+    {
+        _moveProcessor.Initialize(transform);
+        _weaponProcessor.Initialize(transform);
+        // Increment enemies counter
+    }
+
+    private void Update()
+    {
+        _moveProcessor.TickCommandThreads();
+        _weaponProcessor.TickCommandThreads();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (_stats.TakeDamage(damage))
+        {
+            // Add amount of points _stats.PointsForKill;
+            // Die
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Decrement enemies counter
+        // Delete itself
+    }
+
+    // Тут еще должен быть коллайдер или триггер, но он может быть и на пулях, хз
+    // Возможно, что пули будут делать только урон попаданиями, а этот будет делать урон при контакте
+
+    //private void OnCollisionEnter2D(Collision2D collision) // Maybe OnCollisionStay even
+    //{
+    //    if collide with player
+    //      deal _stats.DamageOnContact damage
+    //}
+}
---
+++ Helius Nerus/Assets/Scripts/Enemies/Enemies Architecture/EnemyStats.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class EnemyStats
+{
+    public int PointsForKill
+    {
+        get => _pointsForKill;
+    }
+    public int DamageOnContact
+    {
+        get => _damageOnContact;
+    }
+
+    [Tooltip("Количество ХП врага")]
+    [SerializeField] private float _Health = 1.0f;
+    [Tooltip("Количество очков за убийство врага")]
+    [SerializeField] private int _pointsForKill = 10;
+    [Tooltip("Наносимый урон при контакте")]
+    [SerializeField] private int _damageOnContact = 1;
+
+    public bool TakeDamage(float damage)
+    {
+        _Health -= damage;
+        if (_Health <= 0.0f)
+            return true;
+        return false;
+    }
+}
