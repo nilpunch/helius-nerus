5d13e2bbf9ab37f3984cb8a8431ece5fa04ce2c4
BulkaWithMaslo <slojnoblet@gmail.com>
2020-10-02 10:39:17 +0300

    Add fps counter

---
+++ Helius Nerus/Assets/FPSCounter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FPSCounter : MonoBehaviour
+{
+    private Text _text = null;
+
+    private void Awake()
+    {
+        _text = GetComponent<Text>();
+        Application.targetFrameRate = 1000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _text.text = "FPS " + (int)(1.0f / Time.deltaTime);   
+    }
+}
