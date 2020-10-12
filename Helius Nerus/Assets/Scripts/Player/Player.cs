4b1ed815d60baa7d6b8a70ad94417b4f7a76d85d
Daniil Pankevich <daniil.pankevich@gmail.com>
2020-10-12 19:08:13 +0300

    Slight refactor Player and add some events

--- Helius Nerus/Assets/Scripts/Player/Player.cs
+++ Helius Nerus/Assets/Scripts/Player/Player.cs
@@ -3,82 +3,90 @@
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-    public static event System.Action PlayerTookDamage = delegate { };
+	public static event System.Action<Player> PlayerTookDamage = delegate { };
+	public static event System.Action<Player> PlayerBeforeDie = delegate { };
+	public static event System.Action<Player> PlayerDie = delegate { };
+	public static event System.Action<Player> PlayerResurrection = delegate { };
 
-    [SerializeField] private int _maxHealth = 4;
-    [SerializeField] private float _invinsibilityTime = 1.0f;
-    [Tooltip("Список пушек персонажа")]
-    [SerializeField] private PlayerWeapon[] _weapons;
-    private int _health = 4;
-    private float _invinsibilityLeft = 0.0f;
-    private bool _isInvincible = false;
+	[SerializeField] private PlayerParameters _playerParameters = null;
 
-    private Rigidbody2D _rigidbody = null;
+	public PlayerParameters PlayerParameters
+	{
+		get => _playerParameters;
+	}
 
-    private void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody2D>();
+	private float _invinsibilityLeft = 0.0f;
+	private bool _isInvincible = false;
 
-        PlayerTookDamage += BecomeInvincible;
+	private Rigidbody2D _rigidbody = null;
 
-        _health = _maxHealth;
-    }
+	private void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+		_playerParameters.CurrentHelath = _playerParameters.MaxHealth;
+	}
 
-    private void Update()
-    {
-        if (_isInvincible)
-        {
-            _invinsibilityLeft -= Time.deltaTime;
-            if (_invinsibilityLeft <= 0.0f)
-            {
-                _isInvincible = false;
-                _rigidbody.simulated = true;
+	private void Update()
+	{
+		if (_isInvincible)
+		{
+			_invinsibilityLeft -= Time.deltaTime;
+			if (_invinsibilityLeft <= 0.0f)
+			{
+				_isInvincible = false;
+				_rigidbody.simulated = true;
 #if UNITY_EDITOR
-                gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+				gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
 #endif
-            }
-        }
-    }
+			}
+		}
+	}
 
-    private void BecomeInvincible()
-    {
-        _invinsibilityLeft = _invinsibilityTime;
-        _isInvincible = true;
-        _rigidbody.simulated = false; // Disable rigidbody (collider), so player is not able to take damej
-                                      // For demonstration purposes
+	private void BecomeInvincible()
+	{
+		_invinsibilityLeft = _playerParameters.InvinsibilityTime;
+		_isInvincible = true;
+		_rigidbody.simulated = false; // Disable rigidbody (collider), so player is not able to take damej
+									  // For demonstration purposes
 #if UNITY_EDITOR
-        gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+		gameObject.GetComponent<SpriteRenderer>().color = Color.green;
 #endif
-    }
+	}
 
-    private void TakeDamage(int damage)
-    {
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		IDealDamageToPlayer dealDamageToPlayer = (collision.gameObject.GetComponent(typeof(IDealDamageToPlayer)) as IDealDamageToPlayer);
+		if (dealDamageToPlayer != null)
+		{
+			TakeDamage(dealDamageToPlayer.GetMyDamage());
+		}
+	}
+
+	private void TakeDamage(int damage)
+	{
 		if (_isInvincible)
 			return;
 
-        _health -= damage;
-        if (_health <= 0)
-        {
-            // die script
-            Destroy(gameObject);
-        }
-        else
-        {
-            PlayerTookDamage.Invoke();
-        }
-    }
+		if (damage >= 0)
+		{
+			_playerParameters.CurrentHelath -= damage;
+			PlayerTookDamage.Invoke(this);
+			BecomeInvincible();
+		}
 
-    private void OnDestroy()
-    {
-        PlayerTookDamage -= BecomeInvincible;
-    }
+		if (_playerParameters.CurrentHelath <= 0)
+		{
+			PlayerBeforeDie.Invoke(this);
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        IDealDamageToPlayer dealDamageToPlayer = (collision.gameObject.GetComponent(typeof(IDealDamageToPlayer)) as IDealDamageToPlayer);
-        if (dealDamageToPlayer != null)
-        {
-            TakeDamage(dealDamageToPlayer.GetMyDamage());
-        }
-    }
+			if (_playerParameters.CurrentHelath <= 0)
+			{
+				PlayerDie.Invoke(this);
+				Destroy(gameObject);
+			}
+			else
+			{
+				PlayerResurrection.Invoke(this);
+			}
+		}
+	}
 }
---
+++ Helius Nerus/Assets/Scripts/Player/PlayerParameters.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerParameters : ScriptableObject
+{
+	[Tooltip("Максимальное количество здоровья")]
+	[SerializeField] private int _maxHealth = 4;
+	[Tooltip("Время неуязвимости")]
+	[SerializeField] private float _invinsibilityTime = 1.0f;
+	[Tooltip("Список пушек персонажа")]
+	[SerializeField] private PlayerWeapon[] _weapons = null;
+
+	private int _currentHelath = 0;
+
+	public int CurrentHelath { get => _currentHelath; set => _currentHelath = value; }
+	public int MaxHealth { get => _maxHealth; set => _maxHealth = value; }
+	public float InvinsibilityTime { get => _invinsibilityTime; set => _invinsibilityTime = value; }
+	public PlayerWeapon[] Weapons { get => _weapons; set => _weapons = value; }
+}
