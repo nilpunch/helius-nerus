0c804fc89edb6aae37440d8dd4be803b6f3c57ef
BulkaWithMaslo <slojnoblet@gmail.com>
2020-10-21 12:03:06 +0300

    WIP

---
+++ Helius Nerus/Assets/Scripts/Level/InGameLevelLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class InGameLevelLoader : MonoBehaviour
+{
+    private void Awake()
+    {
+        // temp
+        int currentLevel = 0;
+
+        Resources.Load<GameObject>("Levels/Level_" + currentLevel);
+
+        Destroy(gameObject);
+    }
+}
