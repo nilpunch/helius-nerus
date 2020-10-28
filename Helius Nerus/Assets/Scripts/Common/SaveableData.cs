70fa0350b80844f8932fdaddc1d0c789d225e3c6
BulkaWithMaslo <slojnoblet@gmail.com>
2020-10-28 16:33:47 +0300

    Add saving and loading to SaveableData
    Not tested though

--- Helius Nerus/Assets/Scripts/Common/SaveableData.cs
+++ Helius Nerus/Assets/Scripts/Common/SaveableData.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
 
 public class SaveableData : MonoBehaviour
 {
@@ -10,11 +13,12 @@ public class SaveableData : MonoBehaviour
 
     public int MaximalUnlockedLevel
     {
-        get => _maximalUnlockedLevel;
+        get => _allSaveableFields._maximalUnlockedLevel;
     }
     private ScoreCounter _score = new ScoreCounter();
-    private int _maximalUnlockedLevel = 0;
-    private int _totalScore = 0;
+
+    private AllSaveableFields _allSaveableFields = new AllSaveableFields();
+
 
     private void Awake()
     {
@@ -32,20 +36,107 @@ public class SaveableData : MonoBehaviour
     public void AddMaximalLevels(int amount)
     {
         // Boss has this property? 4 for all bosses, 1 for pre-last and 0 for final?
-        _maximalUnlockedLevel += amount;
+        _allSaveableFields._maximalUnlockedLevel += amount;
     }
     public void AddTotalScore(int amount)
     {
-        _totalScore += amount;
+        _allSaveableFields._totalScore += amount;
     }
 
     private void SaveDate()
     {
-        // write
+        string fileName = "SaveableData.cpl";
+        string filePath = Path.Combine(Application.persistentDataPath, fileName);
+        string dataAsJson = JsonUtility.ToJson(_allSaveableFields);
+
+        if (!string.IsNullOrEmpty(filePath))
+        {
+#if UNITY_EDITOR
+            Debug.LogError("Saveable data can't access data path!");
+#endif
+            return;
+        }
+
+        if (Application.isMobilePlatform) // android and so on
+        {
+            // idk, should work
+            File.WriteAllText(filePath, dataAsJson);
+        }
+        else // pc - editor in our case
+        {
+            File.WriteAllText(filePath, dataAsJson);
+        }
+
     }
 
     private void LoadData()
     {
-        // read assing
+        string fileName = "SaveableData.cpl";
+        string filePath = Path.Combine(Application.persistentDataPath, fileName);
+
+        string dataAsJson = "";
+        //Android
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            //Какая-то счтука, которая может читать на андроиде файлы
+            UnityWebRequest www = UnityWebRequest.Get(filePath);
+
+            //Читаем
+            www.SendWebRequest();
+
+            //error
+            if (www.isNetworkError || www.isHttpError)
+            {
+                return;
+            }
+            else
+            {
+                //not error
+                //Не знаю зачем, написали надо
+                while (!www.isDone) ;
+                //А тут текст получаем
+                dataAsJson = www.downloadHandler.text;
+            }
+
+        }
+        //PC (for now)
+        else
+        {
+            if (File.Exists(filePath))
+            {
+                //Прочитали текст
+                dataAsJson = File.ReadAllText(filePath);
+            }
+            else
+            {
+#if UNITY_EDITOR
+                //Файла нету
+                Debug.LogError("Error - can't acces saveable data file!");
+#endif
+                return;
+            }
+        }
+        if (dataAsJson == "")
+        {
+            _allSaveableFields = new AllSaveableFields();
+        }
+        else
+        {
+            _allSaveableFields = JsonUtility.FromJson<AllSaveableFields>(dataAsJson);
+        }
     }
+}
+
+[System.Serializable]
+public class AllSaveableFields
+{
+    public int _amountOfMoney = 0;
+    public List<int> _unlockedShips = new List<int>();
+    public List<int> _boughtShips = new List<int>();
+    public int _maximalScore = 0;
+    public int _totalScore = 0;
+    public List<int> _unlockedArtifacts = new List<int>();
+    public int _maximalUnlockedLevel = 0;
+    public List<int> _openedWeaponModifiers = new List<int>();
+    public int _donatedToMonument = 0;
 }
