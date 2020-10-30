49170444c8d0ce4b6ce8ef9283b3b525ec738b5e
BulkaWithMaslo <slojnoblet@gmail.com>
2020-10-30 09:15:09 +0300

    Add reflection achievements

---
+++ Helius Nerus/Assets/Scripts/Achievments/AchievementSystem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class AchievementSystem : MonoBehaviour
+{
+    public static AchievementSystem Instance
+    {
+        get => _instance;
+    }
+
+    [SerializeField] private Achievment[] _achievments = null;
+
+    private static AchievementSystem _instance = null;
+
+    private void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        LoadAchievements();
+
+        foreach (Achievment achievment in _achievments)
+        {
+            achievment.Init();
+        }
+    }
+
+    private void SaveAchievments()
+    {
+        return;
+    }
+
+    private void LoadAchievements()
+    {
+        return;
+    }
+
+}
