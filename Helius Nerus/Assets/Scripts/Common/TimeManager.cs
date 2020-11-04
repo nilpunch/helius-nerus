cb17ab595888af7ad3f3b913638685cda9651838
BulkaWithMaslo <slojnoblet@gmail.com>
2020-11-04 13:09:55 +0300

    Add static TimeManager

---
+++ Helius Nerus/Assets/Scripts/Common/TimeManager.cs
@@ -0,0 +1,26 @@
+﻿public static class TimeManager
+{
+    public static float EnemyTimeScale
+    {
+        get;
+        set;
+    } = 1.0f;
+
+    public static float PlayerTimeScale
+    {
+        get;
+        set;
+    } = 1.0f;
+
+    public static float AnimationTimeScale
+    {
+        get;
+        set;
+    } = 1.0f;
+
+    public static float UITimeScale
+    {
+        get;
+        set;
+    } = 1.0f;
+}
