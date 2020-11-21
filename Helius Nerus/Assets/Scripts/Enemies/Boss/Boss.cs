b0b18480a36c1f22ed5da2a4522c8eacc239c2f5
Daniil Pankevich <daniil.pankevich@gmail.com>
2020-11-21 13:43:30 +0300

    New player and bullets with particles

--- Helius Nerus/Assets/Scripts/Enemies/Boss/Boss.cs
+++ Helius Nerus/Assets/Scripts/Enemies/Boss/Boss.cs
@@ -1,5 +1,16 @@
 ﻿using UnityEngine;
 
-public class Boss : MonoBehaviour
+public class Boss : MonoBehaviour, IDealDamageToPlayer, ITakeDamageFromPlayer
 {
+	public int Damage { get; set; }
+
+	public int GetMyDamage()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void TakeDamage(float damage)
+	{
+		throw new System.NotImplementedException();
+	}
 }
